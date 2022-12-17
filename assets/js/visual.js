f804df2a4bb63ede35eb855b36e0effb58064ba3
Anitta P Markose <anittapm1708@gmail.com>
2022-12-17 15:12:41 +0530

    edited index.html

---
+++ assets/js/visual.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+
+    window.matchMedia('(prefers-color-scheme: dark)').matches ? switchNi() : switchDay();
+
+}
+
+function switchNi() {
+    nmode = true;
+    let buttonNi = document.getElementById("night");
+    if (buttonNi.style.backgroundColor == "rgb(24, 44, 57)")
+        return;
+
+    let buttonDay = document.getElementById("day");
+
+    buttonDay.style.backgroundColor = "#030c12";
+    buttonNi.style.backgroundColor = "#182c39";
+
+    document.getElementById("alter").href = "";
+}
+
+function switchDay() {
+    nmode = false;
+    let buttonDay = document.getElementById("day");
+    if (buttonDay.style.backgroundColor == "rgb(24, 44, 57)")
+        return;
+
+    let buttonNi = document.getElementById("night");
+
+    buttonNi.style.backgroundColor = "#030c12";
+    buttonDay.style.backgroundColor = "#182c39";
+
+    document.getElementById("alter").href = "assets/css/productlight.css";
+
+}
